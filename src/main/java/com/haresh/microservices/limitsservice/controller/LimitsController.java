19e653abd2d1a629b89deda2f05499a24b6742fc
Haresh Shaha <haresh.shaha7@gmail.com>
2021-10-31 16:49:21 +0530

    picked properties from configuration

--- src/main/java/com/haresh/microservices/limitsservice/controller/LimitsController.java
+++ src/main/java/com/haresh/microservices/limitsservice/controller/LimitsController.java
@@ -1,13 +1,20 @@
 package com.haresh.microservices.limitsservice.controller;
 
 import com.haresh.microservices.limitsservice.bean.Limits;
+import com.haresh.microservices.limitsservice.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
+
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping("limits")
     public Limits retrieveLimits() {
-        return new Limits(1, 1000);
+//        return new Limits(1, 1000);
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
     }
 }
